683e0a3dbb1403026e2be48b6ae56a1a0aa3dfa7
ValdyBeniah <valdybeniah12@gmail.com>
2024-06-29 01:25:27 +0700

    Revert "Commit changes in frontend directory"
    This reverts commit 71df1fc0eac05c205f8595a3db8a824f42695983.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Login from "./component/Login";
-import Users from "./pages/Users";
-import Absensi from "./pages/Absensi";
-import AddUsers from "./pages/AddUsers";
-import EditUsers from "./pages/EditUsers";
-import AddAbsensi from "./pages/AddAbsensi";
-import EditAbsensi from "./pages/EditAbsensi";
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/users/add" element={<AddUsers />} />
-          <Route path="/users/edit/:id" element={<EditUsers />} />
-          <Route path="/wfh" element={<Absensi />} />
-          <Route path="/wfh/add" element={<AddAbsensi />} />
-          <Route path="/wfh/edit/:id" element={<EditAbsensi />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
     </div>
   );
 }
